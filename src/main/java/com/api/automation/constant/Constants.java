56eb6389a3c2451bad909e303ffbcf89ec0f2e36
prem8483-kumar <121670511+prem8483-kumar@users.noreply.github.com>
2026-04-23 11:37:26 +0530

    Delete src directory

--- src/main/java/com/api/automation/constant/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.api.automation.constant;
-
-import com.api.automation.util.ReadConfig;
-
-public class Constants {
-
-    public final static String USER_SERVICE_BASE_URL = ReadConfig.getProperty("user.service.baseUrl");
-    public final static String PET_SERVICE_BASE_URL = ReadConfig.getProperty("pet.service.baseUrl");
-}
